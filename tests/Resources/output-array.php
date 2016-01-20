b0c08e0c8291e34fdf74afee5747e7b5ed77f6f7
Davide Borsatto <davide.borsatto@gmail.com>
2016-01-20 22:02:32 +0100

    Fixed test output array indentation

--- tests/Resources/output-array.php
+++ tests/Resources/output-array.php
@@ -1,28 +1,28 @@
 <?php
 
 return array(
-  'name' => 'note',
-  'value' => '',
-  'attributes' => array(
-    'to' => 'John',
-    'from' => 'Jane',
-  ),
-  'children' => array(
-    0 => array(
-      'name' => 'subject',
-      'value' => 'Reminder',
-      'attributes' => array(
-      ),
-      'children' => array(
-      ),
+    'name' => 'note',
+    'value' => '',
+    'attributes' => array(
+        'to' => 'John',
+        'from' => 'Jane',
     ),
-    1 => array(
-      'name' => 'body',
-      'value' => 'Don\'t forget me this weekend!',
-      'attributes' => array(
-      ),
-      'children' => array(
-      ),
+    'children' => array(
+        0 => array(
+            'name' => 'subject',
+            'value' => 'Reminder',
+            'attributes' => array(
+            ),
+            'children' => array(
+            ),
+        ),
+        1 => array(
+            'name' => 'body',
+            'value' => 'Don\'t forget me this weekend!',
+            'attributes' => array(
+            ),
+            'children' => array(
+            ),
+        ),
     ),
-  ),
 );
